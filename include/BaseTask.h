0a35c36f48ecce62c507af567c7a1e0fa59e56bb
huangchen <huangchen_123@qq.com>
2023-07-11 19:37:18 +0800

    添加了部分注释，详细说明了如何编写任务类

--- include/BaseTask.h
+++ include/BaseTask.h
@@ -2,17 +2,22 @@
 #ifndef _BASETASK_H
 #define _BASETASK_H
 #include <unistd.h>
+#include <memory>
+#include "Epoll.h"
+#include "Timer.h"
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include "noncopyable.h"
+using std::shared_ptr;
 // #define TaskType std::remove_reference<decltype(*this)>::type;
 
 /*
     任务类编写说明：
     首先一定要继承基类BaseTask
-    一个任务类对象的智能指针存在于定时器和Epoll的fd2task中。
 
+    一个任务类对象的智能指针存在于定时器和Epoll的fd2task中。
     Epoll处理时不会改动定时器和fd2task中的任务指针
+        任务类需要自己管理定时器和epoll监测事件
         所以如果任务处理时希望更新该任务的定时器，需要与定时器双向解耦，然后重新添加
         如果希望与客户端断开连接，需要和定时器双向解耦并手动调用epoll_del，对象才能被析构
 
@@ -22,6 +27,12 @@
 
 class BaseTask
 {
+    /*
+        应该包含如下类型名声明：
+        using SP_TimerManager = shared_ptr<TimerManager<TaskType>>;
+        using SP_Timer = shared_ptr<TimerNode<TaskType>>;
+        using SP_Epoll = shared_ptr<Epoll<TaskType>>;
+    */
 public:
     BaseTask() = delete;
     BaseTask(int sock, sockaddr_in addr): sock_(sock), addr_(addr){}
@@ -31,11 +42,12 @@ public:
     sockaddr_in getaddr() const {return addr_;}
     /*
         在其派生类中应该定义以下成员函数：
-        TaskType(int sock);   构造函数
+        TaskType(int sock, sockaddr_in addr);   构造函数
         void linkTimer(SP_Timer timer);
         void separateTimer();   // 与定时器单向解耦，即定时器删除时会连带删除任务，任务类中通常不调用这个函数
         void bilateralSeparateTimer(); // 与定时器双向解耦，即定时器删除时不会删除任务
         static void Init(SP_TimerManager, SP_Epoll);
+        void process() override;  业务函数，必须重新的纯虚函数
     */
 
 protected:
